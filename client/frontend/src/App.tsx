98ce6d1f10bd3e5c5149adef566d7e885b796cb7
Tushar <tusharba321@gmail.com>
2025-01-18 09:38:06 +0530

    Created project setup and dependencises setup of frontend backend and smart contract part

---
+++ client/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+
+function App() {
+  function TypographyH1() {
+    return (
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+        NFT RPG GAME LOADING.......
+      </h1>
+    )
+  }
+  
+  return (
+    <>
+    <div className="bg-background text-foreground flex items-center justify-center h-screen" >
+    <TypographyH1/>
+    </div>  
+    </>
+  )
+}
+
+export default App
